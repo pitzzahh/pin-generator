5d6eecabf3f15f8f6703459e2769fa6903332cd3
pitzzahh <araopj2191@gmail.com>
2022-07-11 16:47:35 +0800

    removed Test.

--- src/test/java/com/pitzzahh/PinGeneratorTest.java
+++
@@ -1,8 +0,0 @@
-package com.pitzzahh;
-
-class PinGeneratorTest {
-    public static void main(String[] args) {
-        PinGenerator.Pin pin = PinGenerator.generatePin(PinGenerator.NUMBERS, 4);
-        System.out.println(pin);
-    }
-}
